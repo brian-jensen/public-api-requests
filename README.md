9df4e23dbec2237704ff8b10fe3e85b4c55067dd
“Brian” <31598338+brian-jensen@users.noreply.github.com>
2018-10-15 13:00:49 -0700

    added additional styles and code comments

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 **Brian Jensen's Public API Requests - Employee Directory**
 
 ---
+
+## Style Changes
+
+* Employee name color change.
+* Body background image.
+* Card hover box-shadow and border color change at 768px and 1024px breakpoints.
+* Changed header h1 font-size for each screen size.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 6 */
 
+// Fetches 12 random user objects from randomuser.me and passes them to directory()
 fetch('https://randomuser.me/api/?results=12&nat=us')
   .then(response => response.json())
   .then(data => directory(data.results))
@@ -8,11 +9,16 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
 
 document.querySelector('body').innerHTML += `<div class="modal-container"></div>`;
 
+/**
+ * Builds employee info card gallery
+ * @param {array} employees - Any array of 12 random employee objects.
+ */
 const directory = employees => {
   const gallery = document.querySelector('#gallery');
   for (let index in employees) {
     let employee = employees[index];
     let _state = abbrState(employee.location.state);
+    
     gallery.innerHTML += `
       <div class="card">
         <div class="card-img-container">
@@ -26,7 +32,7 @@ const directory = employees => {
       </div>
     `;
   }
-
+  // Adds 'click' event handler to each employee info card. Calls modal() and passes info regarding the employee that was clicked.
   gallery.querySelectorAll('.card').forEach((card, index) => {
     card.addEventListener('click', () => {
       modal(employees, employees[index], index);
@@ -34,11 +40,16 @@ const directory = employees => {
   });
 };
 
-
+/**
+ * Displays modal and employee modal info.
+ * @param {array} employees - Any array of 12 employee objects.
+ * @param {object} employee - Object containing the clicked employees info.
+ * @param {number} index - Index position in array.
+ */
 const modal = (employees, employee, index) => {
   const modalContainer = document.querySelector('.modal-container');
-  const dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language);
-  const _state = abbrState(employee.location.state);
+  const dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language); // Formats date depending on users locale.
+  const _state = abbrState(employee.location.state); // Changes state name from full name to 2 letter abbreviation.
   modalContainer.style.display = 'block';
   modalContainer.innerHTML = `
     <div class="modal">
@@ -59,22 +70,23 @@ const modal = (employees, employee, index) => {
       <button type="button" id="modal-next" class="modal-next btn">Next</button>
     </div> 
   `;
-
+  // Closes modal when 'X' is clicked
   document.querySelector('.modal-close-btn').addEventListener('click', () => {
     modalContainer.style.display = 'none';
   });
 
+/* MODAL NAVIGATION */
   const len = employees.length - 1;
-
+// Navigates modal to previous employee. If clicked while on first employee, displays last employee.
   document.querySelector('.modal-prev').addEventListener('click', () => {
     return index > 0 ? modal(employees, employees[index - 1], index - 1) : modal(employees, employees[len], len);
   });
-
+// Navigates modal to next employee. If clicked while on last employee, displays first employee.
   document.querySelector('.modal-next').addEventListener('click', () => {
     return index < len ? modal(employees, employees[index + 1], index + 1) : modal(employees, employees[0], 0);
   });
 };
-
+// Displays modal navigation.
 document.querySelector('.search-container').innerHTML = `
   <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -82,6 +94,7 @@ document.querySelector('.search-container').innerHTML = `
   </form>
 `;
 
+// Handles user name search.
 const handleSearch = () => {
   const cards = document.querySelectorAll('.card');
   const names = document.querySelectorAll('.card-name');
@@ -93,9 +106,9 @@ const handleSearch = () => {
 
 const search = document.querySelector('#search-input');
 const submit = document.querySelector('#search-submit');
-
+// Listens for user to type characters in a name to filter search results.
 search.addEventListener('keyup', handleSearch);
-
+// Listens for user to click search submit button to filter search results.
 submit.addEventListener('click', event => {
   event.preventDefault();
   handleSearch();
